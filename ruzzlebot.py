e64c7d643948242ef6a81065ca25e8672082e921
Martin d'Allens <martin.d'allens@intel.com>
2013-04-18 15:46:33 +0200

    Cleanup and working version of the Python script

--- ruzzlebot.py
+++ ruzzlebot.py
@@ -1,24 +1,52 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
-# Make sure the 'tools' folder from your Android SDK is in the PATH for ADB
-# TODO 0 solutions for python3
+# You need ADB in your PATH ('tools' folder from the Android SDK).
+# PIL isn't compatible with python 3 yet.
 
-import sys
-import re
-import zipfile
+
 import os
-import time
-import StringIO
+from PIL import Image, ImageFilter
+import re
 from socket import *
-from PIL import Image
-import ImageFilter
+import sys
+import time
+import zipfile
 
 
-def ord_compat(x): # http://python3porting.com/problems.html#binary-data-in-python-2-and-python-3
+def ord_compat(x):
     if sys.version < '3':
         return ord(x)
     else:
         return x
+def chr_compat(x):
+    if sys.version < '3':
+        return x
+    else:
+        return chr(x)
+
+
+try:
+    from StringIO import StringIO # Python 2
+except ImportError:
+    from io import StringIO
+
+
+try:
+    input = raw_input
+except NameError:
+    pass # Python 2
+
+
+SCORES = { 'A':  1, 'B':  3, 'C':  3, 'D':  2, 'E':  1, 'F':  4, 'G':  2,
+           'H':  4, 'I':  1, 'J':  8, 'K': 10, 'L':  2, 'M':  2, 'N':  1,
+           'O':  1, 'P':  3, 'Q':  8, 'R':  1, 'S':  1, 'T':  1, 'U':  1,
+           'V':  5, 'W': 10, 'X': 10, 'Y': 10, 'Z': 10 }
+
+COLORS = [ (236, 237, 236, (1, 1)), # R, G, B, bonus (L, W)
+           (45,  169,  36, (2, 1)),
+           (55,  122, 180, (3, 1)),
+           (241, 199,  88, (1, 2)),
+           (228,  71,  71, (1, 3)) ]
 
 
 def adb_command(cmd):
@@ -57,16 +85,10 @@ def monkey_command(sock, cmd):
     sock.flush() # It's a file, not a socket, so it needs to be flushed.
     
     response = sock.readline()
-    if response.startswith('OK:'):
+    if response.startswith('OK'):
         return response[3:-1]
-    elif response.startswith('OK'):
-        return True
-    elif response.startswith('ERROR'):
-        print(response)
-        return False
     else:
-        print('Unknown command')
-        return False
+        raise IOError(response)
 
 
 def monkey_getvar(sock, var):
@@ -77,7 +99,7 @@ def get_screenshot():
     png = adb_command('shell screencap -p')
     png = png.replace('\r\n', '\n') # ADB inserts carriage returns.
     
-    return Image.open(StringIO.StringIO(png))
+    return Image.open(StringIO(png))
 
 
 def process_screen():
@@ -106,40 +128,35 @@ def process_screen():
     corner  = (left[0] + cell[0] / 2, top[1] + cell[1] / 2) # Center of the first cell.
     bonuses = [[(1, 1)] * 4 for j in range(4)]
 
-    img2=img.load()
+    pixels = img.load()
     for i in range(4):
         for j in range(4):
-            # Typical values
-            # 236, 237, 236 None
-            # 55,  122, 180 TL
-            # 241, 199,  88 DW
-            # 228,  71,  71 TW
-            # 45,  169,  36 DL
-
-            pixel = img.getpixel((corner[0] + (j - .45) * cell[0],
-                                  corner[1] + (i - .45) * cell[1]))
+            rgba = pixels[corner[0] + (j - .45) * cell[0],
+                          corner[1] + (i - .45) * cell[1]]
             
-            #~ if pixel[3] < 150:
-                #~ if pixel[]
-            #~ elif pixel[]: # TODO
+            pixels[corner[0] + (j - .45) * cell[0],
+                   corner[1] + (i - .45) * cell[1]] = (0, 0, 0, 255) # Debug.
             
-            img2[corner[0] + (j - .45) * cell[0],
-                 corner[1] + (i - .45) * cell[1]] = (0, 0, 0)
-            print(pixel)
-    img.show()
+            m = min(COLORS, key=lambda (r, g, b, _):
+                                    abs(rgba[0] - r)
+                                  + abs(rgba[1] - g)
+                                  + abs(rgba[2] - b))
+            
+            bonuses[i][j] = m[3]
+            sys.stdout.write({ (1, 1): " []",
+                               (2, 1): " DL",
+                               (3, 1): " TL",
+                               (1, 2): " DW",
+                               (1, 3): " TW"  }[m[3]]); # Display the bonuses.
+        print('')
+    
     return (cell, corner, bonuses)
-
-
-SCORES = { 'A':  1, 'B':  3, 'C':  3, 'D':  2, 'E':  1, 'F':  4, 'G':  2,
-           'H':  4, 'I':  1, 'J':  8, 'K': 10, 'L':  2, 'M':  2, 'N':  1,
-           'O':  1, 'P':  3, 'Q':  8, 'R':  1, 'S':  1, 'T':  1, 'U':  1,
-           'V':  5, 'W': 10, 'X': 10, 'Y': 10, 'Z': 10  };
 
 
 def input_board():
     clean = []
     while len(clean) < 16:
-        clean += re.findall('[A-Z]', raw_input().upper())
+        clean += re.findall('[A-Z]', input().upper())
 
     return [[clean[i + j * 4] for i in range(4)] for j in range(4)]
 
@@ -155,7 +172,7 @@ def explore(board, input_f, index, i, j, current_path, paths):
     
     # Try to find a sibling with this letter.
     while True:
-        if input_f[index * 4] == letter:
+        if chr_compat(input_f[index * 4]) == letter:
             current_path = list(current_path) # Cloning.
             current_path.append( (i, j) )
 
@@ -215,7 +232,7 @@ def type_solution(sock, cell, corner, solution):
 
 
 def main():
-    print('Waiting for device...')
+    print('Waiting for a phone...')
     adb_command('wait-for-device')
     
     f = open_dictionary('fr')
@@ -223,7 +240,7 @@ def main():
     input_f = f.read()[8:] # Skip the header.
     f.close()
     
-    print('What are the letters?')
+    print('What are the 16 letters?')
     board = input_board()
     
     print('Exploring board...')
@@ -247,17 +264,19 @@ def main():
     
     print('Playing...')
     words = set()
+    score = 0
     for solution in solutions:
         if solution.word in words:
             continue # Skip duplicates.
         
         words.add(solution.word)
         
-        sys.stdout.write(' ' + solution.word)
-        sys.stdout.write(' %d ' % solution.score)
+        sys.stdout.write(solution.word)
+        sys.stdout.write(':%d ' % solution.score)
         sys.stdout.flush()
         type_solution(sock, cell, corner, solution)
-    print('\nDone.')
+        score += solution.score
+    print('\n=> Total score of %d\nDone.' % score)
 
 
 if __name__ == '__main__':
